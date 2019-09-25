d539f299922ecd984ad567d17df004a86dab1fc2
Chloe <gingkolane@gmail.com>
2019-09-25 07:27:47 -0400

    change project name

---
+++ issue-reporter-frontend/src/components/PieChart.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import * as d3 from "d3";
+
+class PieChart extends Component {
+  state = {  }
+  render() { 
+    return ( "Hi" );
+  }
+}
+ 
+export default PieChart;
---
+++ issue-reporter-frontend/src/pages/AnalyticsPage.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Grid, Divider } from 'semantic-ui-react'
+import GraphicsContainer from "../containers/GraphicsContainer";
+import AllReposTable from "../components/AllReposTable";
+import SurveyResultsTable from '../components/SurveyResultsTable';
+import TopNavContainer from "../containers/TopNavContainer";
+
+
+class AnalyticsPage extends Component {
+
+  state = {
+    selectedRepo: {},
+    surveysOfSelectedRepo: []
+  }
+
+  handleTableRepoClick = (repoid) => {
+    let clickedRepo = this.props.repos.find(repo => repo.id === repoid)
+    let surveysForClickedRepo = this.props.surveys.filter(survey => survey.repo_id === repoid)
+    this.setState({ selectedRepo: clickedRepo });
+    this.setState({ surveysOfSelectedRepo: surveysForClickedRepo });
+  }
+
+  render() { 
+    return ( 
+      <>
+
+        <TopNavContainer
+        repos={this.state.repos} //for curriculum dropdown
+        handleTopNavRepoClick={this.handleTopNavRepoClick}  // handle dropdown click
+        currentUser={this.state.currentUser}  //for login
+         />
+
+        <Grid celled>
+          <Grid.Row>
+            
+            <Grid.Column width={16}>
+              <GraphicsContainer
+              repos={this.props.repos} 
+              surveys={this.props.surveys}
+              />
+              <Divider />
+              </Grid.Column>
+            
+            <Grid.Column width={16}>
+              <AllReposTable
+              repos={this.props.repos} 
+              surveys={this.props.surveys}
+              handleTableRepoClick={this.handleTableRepoClick}
+              selectedRepo={this.state.selectedRepo}
+              />
+            </Grid.Column>
+
+            <Grid.Column width={16}>
+              <SurveyResultsTable surveysOfSelectedRepo={this.state.surveysOfSelectedRepo} users={this.props.users} selectedRepo={this.state.selectedRepo}/>
+            </Grid.Column>
+
+          </Grid.Row>
+        </Grid>
+      </>
+     );
+  }
+}
+ 
+export default AnalyticsPage;
