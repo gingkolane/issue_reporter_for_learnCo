e36bd9afe687250d39f2e50e7309957da8574c94
Chloe <gingkolane@gmail.com>
2019-09-26 21:32:52 -0400

    modify repos_user controller

--- app/controllers/repos_users_controller.rb
+++ app/controllers/repos_users_controller.rb
@@ -3,7 +3,6 @@ class ReposUsersController < ApplicationController
   # POST /reposUser
   def create
     reposUser = ReposUser.create(reposUser_params)
-byebug
     if reposUser.valid?
       render json: reposUser
     else
@@ -14,7 +13,7 @@ byebug
   private 
 
   def reposUser_params
-    params.require(:reposUser).permit(:github_repo_id, :github_user_id, :repo_id, :user_id)
+    params.permit(:repo_id, :user_id)
   end
 
 end
