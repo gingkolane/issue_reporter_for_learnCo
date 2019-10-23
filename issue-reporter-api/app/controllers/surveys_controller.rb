9941d02ecd0116b307f3c5674b9c58810c97b3d7
Chloe <gingkolane@gmail.com>
2019-10-22 21:43:14 -0400

    clean up survey's controller

--- issue-reporter-api/app/controllers/surveys_controller.rb
+++ issue-reporter-api/app/controllers/surveys_controller.rb
@@ -1,47 +1,46 @@
 class SurveysController < ApplicationController
-  before_action :set_survey, only: [:show, :update, :destroy, :create]
+  before_action :set_survey, only: [:show, :update, :destroy]
 
   # GET /surveys
   def index
-    @surveys = Survey.all
-
-    render json: @surveys
+    surveys = Survey.all
+    render json: surveys
   end
 
   # GET /surveys/1
   def show
-    render json: @survey
+    render json: survey
   end
 
   # POST /surveys
   def create
-    @survey = Survey.new(survey_params)
-    byebug
-    if @survey.save
-      render json: @survey, status: :created, location: @survey
+    # byebug
+    survey = Survey.new(survey_params)
+    if survey.save
+      render json: survey, status: :created, location: survey
     else
-      render json: @survey.errors, status: :unprocessable_entity
+      render json: survey.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /surveys/1
   def update
-    if @survey.update(survey_params)
-      render json: @survey
+    if survey.update(survey_params)
+      render json: survey
     else
-      render json: @survey.errors, status: :unprocessable_entity
+      render json: survey.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /surveys/1
   def destroy
-    @survey.destroy
+    survey.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_survey
-      @survey = Survey.find(params[:id])
+      survey = Survey.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
