ec0637a7d3c669d273af16f9a58997823e5307bc
Chloe <gingkolane@gmail.com>
2019-10-22 21:18:54 -0400

    clean up repos controller

--- issue-reporter-api/app/controllers/repos_controller.rb
+++ issue-reporter-api/app/controllers/repos_controller.rb
@@ -1,43 +1,16 @@
 class ReposController < ApplicationController
 
   # GET /repos
-  def index
+  def index   
     # repos = Repo.all
     repos = Repo.where(cohort_name: 'dumbo-web-051319')
     render json: repos
   end
 
-  def data
-    data = Repo.all.map { |repo| 
-      {
-        "repo_name" => repo.name,
-        "percent_incomplete" => (repo.forks_count - repo.open_issues_count)/repo.forks_count*100,
-        "percent_incompleteColor" => "hsl(169, 70%, 50%)"
-      }
-    }
-    render json: data
-  end
-
   # GET /repos/1
   def show
     repo = Repo.find(params[:id])
-    repo_users = repo.users
-    render json: { repo: repo, users: repo_users }
-  end
-
-  # POST /repos
-  def create
-    @repo = Repo.create(repo_params)
-    render json: @repo
-  end
-
-  # PATCH/PUT /repos/1
-  def update
-    if @repo.update(repo_params)
-      render json: @repo
-    else
-      render json: @repo.errors, status: :unprocessable_entity
-    end
+    render json: repo
   end
 
   private
